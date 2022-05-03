3bef5ff5e6f5d636e8f2cb38fdb79af30cf20e38
fumiya <09.fumi.03@gmail.com>
2022-05-03 09:04:12 +0900

    redux導入

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,63 @@
-import { collection, getDocs } from "firebase/firestore";
-import React from "react";
-import db from './core/firebase';
+import { Box, Button, Typography } from "@mui/material";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  Timestamp,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import db from "./core/Firebase";
 
 export const Home = () => {
+  type Content = {
+    imageUrls: string[];
+    text: string;
+    title: string;
+  };
+
+  type Data = {
+    id: number;
+    createdAt: Timestamp;
+    title: string;
+    contents: Content[];
+  };
+
+  const [contents, setContents] = useState<Data[]>([]);
+  // useEffect(() => {
+
+  // },[]);
   const getColecction = async () => {
-    const getDoc = await getDocs(collection(db, "test"));
+    const getDoc = await getDocs(collection(db, "recipes"));
+    let data = [];
     getDoc.forEach((doc) => {
-      console.log(doc.data().name)
-    })
-  }
+      const collection = doc.data();
+      const result: Data = {
+        id: collection.id,
+        createdAt: collection.createdAt,
+        title: collection.title,
+        contents: collection.contents,
+      };
+      const newContents = [...contents];
+      setContents([...contents, result]);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         sasaki fumiya
-        </p>
-        <button onClick={getColecction}>test</button>
-      </header>
-    </div>
+    <Box className="App">
+      <Button onClick={getColecction}>get</Button>
+      {contents.map((item) => (
+        <Box key={item.id}>
+          <Typography>{item.id}</Typography>
+          <Typography>{item.title}</Typography>
+          {item.contents.map((content, index) => (
+            <Box key={index}>
+              <Typography>{content.text}</Typography>
+              <Typography>{content.title}</Typography>
+              <Typography>{content.imageUrls[0]}</Typography>
+            </Box>
+          ))}
+        </Box>
+      ))}
+    </Box>
   );
 };
 
--- src/components/core/firebase.ts
+++ src/components/core/firebase.ts
@@ -1,20 +1,20 @@
-import firebase, { initializeApp } from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase, { initializeApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 import { Firestore, getFirestore } from "firebase/firestore";
-import 'firebase/storage';
+import "firebase/storage";
 
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const db:Firestore = getFirestore(app);
+ const db: Firestore = getFirestore(app);
 
 export default db;
