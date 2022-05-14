0e7088ab5a8cef411a50c9376ee767f7b19d2395
fumiya <09.fumi.03@gmail.com>
2022-05-14 22:25:48 +0900

    stye inline

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { ViewColumn } from "@mui/icons-material";
 import { Box, Button, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
@@ -18,16 +19,30 @@ export const Home = () => {
   useEffect(() => {
     setContents(recipe);
   }, [recipe]);
+
+  const containerS = {
+    display: "flex",
+    // flexDirection:"column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const titleS = {
+    fontSize: "50px",
+  };
   return (
-    <Box className="App">
+    <Box style={containerS}>
+      <Typography style={titleS}>ももこごはん</Typography>
       <Button onClick={getColecction}>get</Button>
-      {contents.map((item) => (
-        <Box key={item.id}>
-          <Typography>{item.title}</Typography>
-          <Typography>{item.conText}</Typography>
-          <img src={item.mainImageUrl} alt="" style={{ height: "20%" }} />
-        </Box>
-      ))}
+      <Box>
+        {contents.map((item) => (
+          <Box key={item.id}>
+            <Typography>{item.title}</Typography>
+            <Typography>{item.conText}</Typography>
+            <img src={item.mainImageUrl} alt="" style={{ height: "20%" }} />
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
